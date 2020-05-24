01862b7f40a364128aafab5afd311feebcba8f37
sunilpaulmathew <sunil.kde@gmail.com>
2020-05-24 12:02:25 +0200

    Import AnyKernel3 config for SmartPack
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

---
+++ AnyKernel/anykernel.sh
@@ -0,0 +1,51 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=SmartPack-Kernel by sunilpaulmathew@xda-developers.com
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=OnePlus5T
+device.name2=dumpling
+device.name3=OnePlus5
+device.name4=cheeseburger
+device.name5=
+supported.versions=10
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+# init.rc
+
+# init.tuna.rc
+
+# fstab.tuna
+
+# end ramdisk changes
+
+write_boot;
+## end install
